eac95bb5a558521352d6ce745286478594de9c22
kissikek <ermolenkokonstantin1612@gmail.com>
2023-04-14 15:27:01 +0300

    extra lines has been removed

--- Book/Book.cs
+++ Book/Book.cs
@@ -2,134 +2,192 @@
 {
     internal class Book
     {
+        private string _ISBN;
+        private string _author;
+        private string _publishingHouse;
+        private int? _publishingYear;
+        private int? _pages;
+        private decimal? _cost;
+
+        static bool isValidIsbn(string isbn)
+        {
+            int n = isbn.Length;
+            if (n != 10)
+            {
+                return false;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                int digit = isbn[i] - '0';
+                if (0 > digit || 9 < digit)
+                {
+                    return false;
+                }
+                sum += (digit * (10 - i));
+            }
+
+            char last = isbn[9];
+
+            if (last != 'X' && (last < '0' || last > '9'))
+            {
+                return false;
+            }
+
+            sum += ((last == 'X') ? 10 : (last - '0'));
+
+            return (sum % 11 == 0);
+        }
+
         public string ISBN
         {
             get
             {
-                return ISBN;//тут pizda а не формат он зависит даже от года
+                return _ISBN;
             }
             set
             {
-                ISBN = value;
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("value");
+                }
+                else
+                {
+                    if (!isValidIsbn(value))
+                    {
+                        throw new Exception("value");
+                    }
+                    else
+                    {
+                        _ISBN = value;
+                    }
+                }
             }
         }
+
         public string Author
         {
             get
             {
-                return Author;
+                return _author;
             }
             set
             {
-                if (Author != null)
+                if (string.IsNullOrWhiteSpace(value))
                 {
-                    for (int i = 0; i <= 9; i++)
-                    {
-                        if (Author.Contains(Convert.ToChar(i)))
-                        {
-                            throw new CustomException(Author);
-                        }
-                    }
+                    throw new ArgumentNullException("value");
                 }
                 else
                 {
-                    throw new CustomException();
+                    _author = value;
                 }
-                Author = value;
             }
         }
+
         public string PublishingHouse
         {
             get
             {
-                return PublishingHouse;
+                return _publishingHouse;
             }
             set
             {
-                if (value != null)
+                if (string.IsNullOrWhiteSpace(value))
                 {
-                    PublishingHouse = value;
+                    throw new ArgumentNullException("value");
                 }
                 else
                 {
-                    throw new CustomException();
+                    _publishingHouse = value;
                 }
             }
         }
-        public int PublishingYear
+        //посмотреть встроеные эксепшены
+
+        //сначала эксепшн, потом всё ок
+
+        //now,  utc now
+        public int? PublishingYear
         {
             get
             {
-                return PublishingYear;
+                return _publishingYear;
             }
             set
             {
-                if (value != null)
+                if (value is null)
                 {
-                    if (value > 2023 | value < 0)
+                    throw new ArgumentNullException("value");
+                }
+                else
+                {
+                    if (value > DateTime.UtcNow.Year | value < 0)
                     {
-                        throw new CustomException(value, 0, 2023);
+                        throw new ArgumentOutOfRangeException("value");
                     }
                     else
                     {
-                        PublishingYear = value;
+                        _publishingYear = value;
                     }
-                }
-                else
-                {
-                    throw new CustomException();
                 }
             }
         }
-        public int Pages
+
+        public int? Pages
         {
             get
             {
-                return Pages;
+                return _pages;
             }
             set
             {
-                if (value != null)
+                if (value is null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+                else
                 {
                     if (value <= 0)
                     {
-                        throw new CustomException(value, 0);
+                        throw new ArgumentOutOfRangeException("value");
                     }
                     else
                     {
-                        Pages = value;
+                        _pages = value;
                     }
-                }
-                else
-                {
-                    throw new CustomException();
                 }
             }
         }
-        public decimal Cost
+
+        public decimal? Cost
         {
             get
             {
-                return Cost;
+                return _cost;
             }
             set
             {
-                if (value != null)
+                if (value is null)
                 {
-                    if (Cost < 0)
-                    {
-                        throw new CustomException(value, 0);
-                    }
-                    else
-                    {
-                        Cost = value;
-                    }
+                    throw new ArgumentNullException("value");
                 }
                 else
                 {
-                    throw new CustomException();
+                    if (value <= 0)
+                    {
+                        throw new ArgumentOutOfRangeException("value");
+                    }
+                    else
+                    {
+                        _cost = value;
+                    }
                 }
             }
         }
     }
 }
+
+
+
+
