ec3484420b4c317d9beb0c8e7f55a406bf7a8413
kissikek <ermolenkokonstantin1612@gmail.com>
2023-04-24 23:06:26 +0300

    asd

---
+++ Book/BookStorage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class BookStogare
+    {
+        //интерфейс IBookStorage
+        private List<Book> _list;
+        public List<Book> List
+        {
+            get
+            {
+                return _list;
+            }
+            set
+            {
+                _list = value;
+            }
+        }
+
+        private void Create()
+        {
+            List<Book> list = new List<Book>();
+            List = list;
+        }
+
+        public void RemoveBook(Book book)
+        {
+            foreach (Book item in List)
+            {
+                if (item.Equals(book))
+                {
+                    List.Remove(item);
+                    return;
+                }
+            }
+        }
+
+        public void FindBookByTeg(Book book)
+        {
+            foreach (Book item in List)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            List.Add(book);
+        }
+    }
+}
